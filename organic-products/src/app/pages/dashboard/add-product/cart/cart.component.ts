2dc758d96578f27d107d05685b05f760b6914995
shekhar_dev <csjoshi143@gmail.com>
2024-03-17 16:14:29 +0530

    changes related responsiveness and table for cart

--- organic-products/src/app/pages/dashboard/add-product/cart/cart.component.ts
+++ organic-products/src/app/pages/dashboard/add-product/cart/cart.component.ts
@@ -4,29 +4,32 @@ import { CommonService } from 'src/app/services/common.service';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit {
+  totalProducts: any;
+  totalCost: number = 0;
+  tableHeaders: any;
 
-  totalProducts : any;
-  totalCost : number = 0;
-
-  constructor(private service: CommonService) { }
+  constructor(private service: CommonService) {}
 
   ngOnInit(): void {
     // this.service.castProduct.subscribe((res:any)=>{
     //   console.log(res)
     //   this.totalProducts = res
     // })
-    this.totalProducts = JSON.parse(sessionStorage.getItem('userItems')!)
-    this.totalCost = this.totalProducts?.map((el:any)=> el?.cost).reduce((a:any,b:any) => a + b , 0)
+    this.totalProducts = JSON.parse(sessionStorage.getItem('userItems')!);
+    this.tableHeaders = Object.keys(this.totalProducts[0]);
+    console.log(this.totalProducts, this.tableHeaders);
+    this.totalCost = this.totalProducts
+      ?.map((el: any) => el?.cost)
+      .reduce((a: any, b: any) => a + b, 0);
   }
 
   clearCart() {
     this.totalCost = 0;
     sessionStorage.clear();
-    this.totalProducts = []
-    this.service.updateProduct([])
+    this.totalProducts = [];
+    this.service.updateProduct([]);
   }
-  
 }
